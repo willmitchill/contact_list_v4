611a74fd829cb248671841324dc807f3215fa058
Will <wmitchel@hotmail.com>
2014-08-12 14:01:22 -0700

    add contact ability

--- public/javascript/application.js
+++ public/javascript/application.js
@@ -1,4 +1,11 @@
 $(document).ready(function() {
-
   // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+    $.ajax({
+      url: '/contacts',
+      method: 'GET',
+    }).done(function (data) {
+      data.forEach(function (d) {
+        $('#contacts-index').append("<p>" + d.first_name + " " + d.last_name + " " + d.email + " " + d.phone_number + "</p>");
+      });
+    });
 });
